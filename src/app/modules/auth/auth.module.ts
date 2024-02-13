666c891a2900a4670fbf5ca8349ae8c854cb5b21
unknown <arpadbacos@gmail.com>
2024-02-13 16:55:23 +0200

    wip

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, FormsModule, AuthRoutingModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, AuthRoutingModule],
 })
 export class AuthModule {}
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+
+  @HostListener('window:keyup.enter', ['$event', 'undefined'])
+  onRegister(): void {
+    if (
+      this.registerForm.controls['password'].value !==
+      this.registerForm.controls['confirmPassword'].value
+    ) {
+      this.registerForm.controls['password'].setErrors({ error: true });
+      this.registerForm.controls['confirmPassword'].setErrors({ error: true });
+
+      return;
+    } else if (
+      this.registerForm.controls['password'].value &&
+      this.registerForm.controls['confirmPassword'].value
+    ) {
+      this.registerForm.controls['password'].setErrors(null);
+      this.registerForm.controls['confirmPassword'].setErrors(null);
+    }
+
+    this.authService
+      .register(this.registerForm.value.email, this.registerForm.value.password)
+      .then(() => {
+        this.router.navigateByUrl('/main');
+      });
+  }
 }
